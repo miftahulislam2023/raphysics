0ad3bd53a11f4cb46eba60be0faf2223cf48f3d5
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2025-03-06 23:10:31 +0600

    tailwind

--- src/app/students/[studentId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function StudentDetail(
-    { params }: { params: Promise<{ studentId: string }> }
-) {
-    const studentId = (await params).studentId;
-    return (
-        <h1>Student details - {studentId}</h1>
-    )
-}
